cf0d2f8a9f7fd351045341446539179a3be9d2dc
shadowjynxs <shadowjynxs@gmail.com>
2023-05-14 03:05:05 +0530

    Initial

--- app/src/main/java/com/nxs/chatopia/LoginActivity.java
+++ app/src/main/java/com/nxs/chatopia/LoginActivity.java
@@ -4,18 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView gotoSignup;
+    EditText lEmail, lPasswd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         gotoSignup = findViewById(R.id.gotoSignup);
+        lEmail = findViewById(R.id.lEmail);
+        lPasswd = findViewById(R.id.lPasswd);
 
         gotoSignup.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), RegisterActivity.class)));
     }
